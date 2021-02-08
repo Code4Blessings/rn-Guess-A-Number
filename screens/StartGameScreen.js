32b80d7c3ec6ea657a475928ec1d29131006a333
Code4Blessings <robincw35@gmail.com>
2021-02-07 23:23:01 -0500

    Added 2 components

---
+++ screens/StartGameScreen.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+const StartGameScreen = props => {
+    return (
+        <View style={styles.screen}>
+            
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({
+    screen: {
+        flex: 1
+    }
+});
+
+export default StartGameScreen;
