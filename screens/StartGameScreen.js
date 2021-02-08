53d631cb48c7addbac7f3293eff17f2c0ba7976b
Code4Blessings <robincw35@gmail.com>
2021-02-08 10:41:49 -0500

    Added some notes for react-native snippets

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -1,10 +1,18 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, TextInput, Button } from 'react-native';
 
 const StartGameScreen = props => {
     return (
         <View style={styles.screen}>
-            <Text>The Game Screen</Text>
+            <Text style={styles.title}>Start A New Game</Text>
+            <View style={styles.inputContainer}>
+                <Text>Select A Number</Text>
+                <TextInput />
+            </View>
+            <View style={styles.buttonContainer}>
+                <Button title='Reset' onPress={() => {}} />
+                <Button  title='Confirm' onPress={() => {}} /> 
+            </View>
         </View>
     );
 };
@@ -15,6 +23,22 @@ const styles = StyleSheet.create({
         padding: 10,
         alignItems: 'center',
         justifyContent: 'flex-start'
+    },
+    title: {
+        fontSize: 20,
+        marginVertical: 10
+    },
+    inputContainer: {
+        width: 300,
+        maxWidth: '80%',
+        alignItems: 'center'
+    },
+    buttonContainer: {
+        flexDirection: 'row',
+        width: '100%',
+        justifyContent: 'space-between',
+        paddingHorizontal: 15
+
     }
 });
 
