5ece877eb5e6c1060ec923a973c65077513dd308
Code4Blessings <robincw35@gmail.com>
2021-02-07 23:30:19 -0500

    Set up the game screen

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -4,14 +4,17 @@ import { View, Text, StyleSheet } from 'react-native';
 const StartGameScreen = props => {
     return (
         <View style={styles.screen}>
-            
+            <Text>The Game Screen</Text>
         </View>
     );
 };
 
 const styles = StyleSheet.create({
     screen: {
-        flex: 1
+        flex: 1,
+        padding: 10,
+        alignItems: 'center',
+        justifyContent: 'flex-start'
     }
 });
 
